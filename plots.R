4a4423961a02e41f9840ccb9ae96c282b9e9bd71
Pascaline <pascaline.descloux@unige.ch>
2019-03-08 16:22:15 +0100

    produced plots

--- plots.R
+++ plots.R
@@ -9,11 +9,25 @@ s.values <- as.numeric(dimnames(results)$sparsity)
 resnames <- dimnames(results)
 # removing MX-knockoffs with offset = 1:
 results <- results[ , , resnames$estimator != "MXknockoffs.off1"]
-# modify estimator names:
 
+# modify estimator names:
 resnames <- dimnames(results)
-pch.ix <- 1:length(resnames$estimator)
-col.ix <- 1:length(resnames$estimator)
+est.names <- resnames$estimator
+est.names <- gsub("lassoZero", "lasso-zero", est.names)
+est.names <- gsub("MXknockoffs.off0", "knockoffs", est.names)
+est.names <- gsub("SLOPE.gauss", "SLOPE", est.names)
+est.names <- gsub("stabilitySelection", "stability selection", est.names)
+est.names <- gsub("wildBinarySeg", "wild binary segmentation", est.names)
+dimnames(results)$estimator <- est.names
+
+# colors and point styles:
+all.est <- c("lasso-zero", "lasso", "SCAD", "stability selection", 
+             "knockoffs", "SLOPE", "wild binary segmentation")
+all.col <-  c("black", "red", "limegreen", "orange", "blue", "magenta", "purple")
+pch.ix <- match(dimnames(results)$estimator, all.est)
+col.ix <- all.col[match(dimnames(results)$estimator, all.est)]
+
+# x-label
 xlab <- expression(s^0)
 
 # support recovery:
@@ -23,10 +37,13 @@ matplot(s.values, results[, resnames$criterion == "supp.recovery", ],
         ylab = expression(P(hat(S) == S^0)), col = col.ix,
         cex.lab=2, cex.axis=1.5, cex.main=2)
 if (type == "smallGauss") {
-    legend(x = 12, y = 1, legend = resnames$estimator, pch = pch.ix,
+    legend(x = 15, y = 1, legend = resnames$estimator, pch = pch.ix,
            lty = 1, col = col.ix, cex=1.3)
 } else if (type == "wideGauss") {
-    legend(x = 10, y = 1, legend = resnames$estimator, pch = pch.ix,
+    legend(x = 8, y = 1, legend = resnames$estimator, pch = pch.ix,
+           lty = 1, col = col.ix, cex=1.3)
+} else if (type == "riboflavin") {
+    legend(x = 6, y = 1, legend = resnames$estimator, pch = pch.ix,
            lty = 1, col = col.ix, cex=1.3)
 } else if(type == "TV300") {
     legend(x=5, y = 1, legend = resnames$estimator, pch = pch.ix,
@@ -46,6 +63,9 @@ matplot(s.values, results[ , resnames$criterion == "tpp", ], type="b",
 title(ylab="TPR", line=3, cex.lab=2)
 if (type == "smallGauss") {
     legend(x = 0, y = 0.6, legend = resnames$estimator, col = col.ix, 
+           lty = 1, pch = pch.ix, cex = 0.75) 
+} else if (type == "riboflavin") {
+    legend(x = 0, y = 0.5, legend = resnames$estimator, col = col.ix, 
            lty = 1, pch = pch.ix, cex = 0.75) 
 } else if (type == "wideGauss") {
     legend(x = 0, y = 0.6, legend = resnames$estimator, col = col.ix, 
